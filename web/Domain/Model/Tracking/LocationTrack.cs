91bf379a809f76d2ffa0138cb676e7f20991745f
Sergiy Bidnyi <sergii@bidnyi.name>
2012-11-10 16:01:05 +0200

    LocationTrack Api added

---
+++ web/Domain/Model/Tracking/LocationTrack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Spatial;
+using Footprint.Domain.Model.Membership;
+
+namespace Footprint.Domain.Model.Tracking
+{
+    public class LocationTrack
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public DbGeography Location { get; set; } 
+    }
+}
---
+++ web/Footprint.Site/Controllers/PaperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Footprint.Site.Controllers
+{
+    public class PaperController : ApiController
+    {
+        // POST api/paper
+        public void Add([FromBody]string value)
+        {
+        }
+    }
+}
