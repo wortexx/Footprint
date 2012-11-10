b273a3ff62a7913c19326ebc945704fddba0b4b7
Valentyn Ponomarenko <bootloader@list.ru>
2012-11-10 19:12:20 +0200

    Commit

--- web/Footprint.Site/Models/AccountModels.cs
+++ web/Footprint.Site/Models/AccountModels.cs
@@ -73,7 +73,7 @@ namespace Footprint.Site.Models
     public class RegisterModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Email")]
         public string UserName { get; set; }
 
         [Required]
@@ -81,11 +81,6 @@ namespace Footprint.Site.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 
     public class ExternalLogin
