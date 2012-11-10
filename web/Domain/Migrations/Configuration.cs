d39b5212fde7ac29dbcacdb465c1222a4a6b485c
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2012-11-11 00:07:01 +0200

    Fixed migration

---
+++ web/Domain/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Footprint.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Footprint.Domain.Model.FootprintContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Footprint.Domain.Model.FootprintContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- web/Domain/Model/FootprintContext.cs
+++ web/Domain/Model/FootprintContext.cs
@@ -7,6 +7,11 @@ namespace Footprint.Domain.Model
 {
     public class FootprintContext : DbContext
     {
+        public FootprintContext()
+            : base("DefaultConnection")
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<LocationTrack> LocationTracks { get; set; }
         public DbSet<PrintingItem> PrintingItems { get; set; }
