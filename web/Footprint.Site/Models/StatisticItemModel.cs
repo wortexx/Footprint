a6e13e6f60ac380a878238755a8b6cad8c178f97
Valentyn Ponomarenko <bootloader@list.ru>
2012-11-11 03:32:49 +0200

    Commit Web

--- web/Footprint.Site/Models/StatisticItemModel.cs
+++ web/Footprint.Site/Models/StatisticItemModel.cs
@@ -4,9 +4,114 @@
     {
         public string Consumer { get; set; }
         public decimal Usage { set; get; }
-        public decimal Norm { set; get; }
-        public bool IsEffective { get { return Usage <= Norm; }
+        public ConsumerType ConsumerType { get; set; }
+        public string UsageInCosumerMeasurements
+        {
+            get
+            {
+                var ret = string.Empty;
+                switch (this.ConsumerType)
+                {
+                    case ConsumerType.Plastic:
+                        {
+                            ret = string.Format("{0} kg.", this.Usage);
+                            break;
+                        }
+                    case ConsumerType.Vehicle:
+                        {
+                            ret = string.Format("{0} km.", this.Usage);
+                            break;
+                        }
+                    case  ConsumerType.Printer:
+                        {
+                            ret = string.Format("{0} pcs", this.Usage);
+                            break;
+                        }
+                    case ConsumerType.Water:
+                        {
+                            ret = string.Format("{0} lt.", this.Usage);
+                            break;
+                        }
+                    case ConsumerType.Electricity:
+                        {
+                            ret = string.Format("{0} kW.", this.Usage);
+                            break;
+                        }
+                    case ConsumerType.Heating:
+                        {
+                            ret = string.Format("{0} kcal.", this.Usage);
+                            break;
+                        }
+                    case ConsumerType.Cooling:
+                        {
+                            ret = string.Format("{0} kW.", this.Usage);
+                            break;
+                        }
+                        
+                }
+                return ret;
+            }
         }
-    
+        public decimal Norm { set; get; }
+        public bool IsEffective { get { return Usage <= Norm; }}
+        public decimal C02
+        {
+            get
+            {
+                //var ret = decimal.Zero;
+                //switch (this.ConsumerType)
+                //{
+                //    case ConsumerType.Plastic:
+                //        {
+                //            ret = this.Usage * 1.5M;
+                //            break;
+                //        }
+                //    case ConsumerType.Vehicle:
+                //        {
+                //            ret = string.Format("{0} km.", this.Usage);
+                //            break;
+                //        }
+                //    case ConsumerType.Printer:
+                //        {
+                //            ret = string.Format("{0} pcs", this.Usage);
+                //            break;
+                //        }
+                //    case ConsumerType.Water:
+                //        {
+                //            ret = string.Format("{0} lt.", this.Usage);
+                //            break;
+                //        }
+                //    case ConsumerType.Electricity:
+                //        {
+                //            ret = string.Format("{0} kW.", this.Usage);
+                //            break;
+                //        }
+                //    case ConsumerType.Heating:
+                //        {
+                //            ret = string.Format("{0} kcal.", this.Usage);
+                //            break;
+                //        }
+                //    case ConsumerType.Cooling:
+                //        {
+                //            ret = string.Format("{0} kW.", this.Usage);
+                //            break;
+                //        }
+
+                //}
+                return this.Usage;
+            }
+        }
+    }
+
+    public enum ConsumerType
+    {
+        None = 0,
+        Vehicle = 1,
+        Printer = 2,
+        Water = 3,
+        Electricity = 4,
+        Heating = 5,
+        Cooling = 6,
+        Plastic = 7
     }
 }
